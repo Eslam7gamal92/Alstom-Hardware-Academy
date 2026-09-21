38a4b8fd26ffe22916b52fb9b3d28c2171e1e0b5
Eslam Gamal <eslam7gamal92@gmail.com>
2026-09-21 12:56:47 +0300

    Add files via upload

--- Alstom HW Academy.py
+++ Alstom HW Academy.py
@@ -829,14 +829,14 @@ HVITC_QUIZ_QUESTIONS = [
 ALSTOM_TOOLS = [
     {
         "title": "Hydra",
-        "description": "An internal Alstom platform that can support engineering and project-related workflows.",
+        "description": "An internal Alstom platform that can support engineering and project-related workflows.                  ",
         "image": "hydra_tool.jpg",
         "tool_link": "https://hydra.bt.bombardier.net/hydra/Client/",
         "learning_link": "https://example.com/hydra-learning"
     },
     {
         "title": "Orchestra",
-        "description": "A useful internal platform that may help users navigate project and collaboration activities.",
+        "description": "A useful internal platform that may help users navigate project and collaboration activities.           ",
         "image": "orchestra_tool.png",
         "tool_link": "https://datamanagement.alstom.com/ebx-ui/ui/custom/Public_Master_Data/action/378",
         "learning_link": "https://alstomgroup.sharepoint.com/sites/My_Data_Journey/SitePages/Your-very-first-steps-in-Orchestra.aspx"
